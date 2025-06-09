226201c56d6e0ad310adce5b938917bc5041b03e
PABITRA KUMAR <pabitrakumar50abc@gmail.com>
2025-06-09 15:44:30 +0530

    Update app.py

--- server/LLM/app/app.py
+++ server/LLM/app/app.py
@@ -131,6 +131,14 @@ origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["https://linker-2-smoky.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(
+    CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
