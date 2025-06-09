b50fbc749592cafba370d1c2cc87d700d351c998
PABITRA KUMAR <pabitrakumar50abc@gmail.com>
2025-06-09 15:47:21 +0530

    Update app.py

--- server/LLM/app/app.py
+++ server/LLM/app/app.py
@@ -137,13 +137,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 # Dependency to get the database session
